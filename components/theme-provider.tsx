db8d9ba9a6426795968ad8538a1fcc3c92253fa6
fadinaber <fadinaber4444@gmail.com>
2025-12-19 15:45:45 +0100

    Remove unused stub components (testimonial-snippet, resizable)

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -1,9 +1,11 @@
 'use client'
 
 import * as React from 'react'
+import {
+  ThemeProvider as NextThemesProvider,
+  type ThemeProviderProps,
+} from 'next-themes'
 
-// ThemeProvider placeholder - next-themes not installed
-// If theme switching is needed, install: npm install next-themes
-export function ThemeProvider({ children, ...props }: { children: React.ReactNode }) {
-  return <>{children}</>
+export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
+  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
 }
--- components/ui/aspect-ratio.tsx
+++ components/ui/aspect-ratio.tsx
@@ -1,25 +1,11 @@
 'use client'
 
-import * as React from 'react'
+import * as AspectRatioPrimitive from '@radix-ui/react-aspect-ratio'
 
-// AspectRatio component - @radix-ui/react-aspect-ratio not installed
-// If needed, install: npm install @radix-ui/react-aspect-ratio
 function AspectRatio({
-  ratio,
-  children,
-  className,
   ...props
-}: React.HTMLAttributes<HTMLDivElement> & { ratio?: number }) {
-  return (
-    <div
-      data-slot="aspect-ratio"
-      className={className}
-      style={{ aspectRatio: ratio ? `${ratio}` : undefined }}
-      {...props}
-    >
-      {children}
-    </div>
-  )
+}: React.ComponentProps<typeof AspectRatioPrimitive.Root>) {
+  return <AspectRatioPrimitive.Root data-slot="aspect-ratio" {...props} />
 }
 
 export { AspectRatio }
